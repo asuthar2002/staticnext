bd5a470b5d851e8bfa5c9ba0f44091db27f2791c
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2025-02-13 12:59:20 +0530

    Update products4.js

--- src/data/products4.js
+++ src/data/products4.js
@@ -23,42 +23,6 @@ const products4 = [
     image: "/images/bw/6.png",
     name: "Pastel Baby Shower Theme",
   },
-  {
-    image: "/images/bw/1.png",
-    name: "Jungle Safari Theme",
-  },
-  {
-    image: "/images/bw/2.png",
-    name: "Whimsical Baby Shower",
-  },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Cute Little Lamb Theme",
-  // },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Nursery Theme Decor",
-  // },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Teddy Bear Baby Shower",
-  // },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Baby's First Party",
-  // },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Baby Bubbles and Balloons",
-  // },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Baby on Board Theme",
-  // },
-  // {
-  //   image: "/images/bp/1.png",
-  //   name: "Under the Sea Baby Shower",
-  // },
 ];
 
 export default products4;

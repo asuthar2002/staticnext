eb6d3691c13cfca4c8dfffe89cbddde5fbed7043
Anil Suthar <97024759+asuthar2002@users.noreply.github.com>
2025-02-13 13:00:23 +0530

    Update products6.js

--- src/data/products6.js
+++ src/data/products6.js
@@ -1,1 +1,24 @@
+const products6 = [
+  {
+    image: "/images/hm/1.jpeg",
+    name: "Classic Welcome Theme 🎈",
+  },
+  {
+    image: "/images/hm/2.jpeg",
+    name: "Floral Elegance Theme 🌸",
+  },
+  {
+    image: "/images/hm/3.jpeg",
+    name: "Travel-Themed Welcome 🌍✈️",
+  },
+  {
+    image: "/images/hm/4.jpeg",
+    name: "Balloon Wall Backdrop 🎭",
+  },
+  {
+    image: "/images/cer/6.jpeg",
+    name: "Grand Balloon Canopy 🎪",
+  },
+];
 
+export default products6;
